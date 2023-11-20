6b12da84cb424caa92860f13255f391571eba12f
Claudio Mora Díaz <xireeell@my.com>
2023-11-20 19:37:39 -0300

    regex and first pack

--- bk/HobL/parser.py
+++ bk/HobL/parser.py
@@ -3,7 +3,6 @@ import re
 class Parser:
 
     ghost_variable = r'^(§|<<)\s@\w+$'
-    assign_string_number = r'^(§|<<)\s\w+(?:>|@)(\(\d+(,\d+)?\)|"[^"]+") #$'
-    basic_function = r'^(§|<<)\s\w+\s(-\w+\s)+(#$|##\s(^@\w+|\w+))$'
-    multi_level_function = r'^(§|<<)\s\w+\s(-+\w+\s)+(#$|##\s(^@\w+|\w+))$'
-    
+    basic_declaration = r'^(§|<<)\s\w+(>|@)(\(\d+(,\d+)?\)|"[^"]+")\s#$'
+    multi_level_function = r'^(§|<<)\s\w+\s(-+\w+\s)+(#|##\s(@\w+|\w+))$'
+    variable_by_function = r'^(§|<<)\s\w+(>|@)((<<|§)\*\*)?\w+\s(-+\w+\s)+(#|##\s@?\w+)$'
